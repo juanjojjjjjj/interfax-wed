7946f83cb482d706728ed2d68d3b8e6151a7511d
nerviozzo <raupulus@gmail.com>
2015-07-29 12:48:43 +0200

    Asignado el uso de GPIO 4

--- Web para controlar GPIO/GPIO/4/apagar.py
+++ Web para controlar GPIO/GPIO/4/apagar.py
@@ -17,10 +17,10 @@ import time
 import os
 
 #Definir GPIO como Salida
-GPIO.setup(1, GPIO.OUT)
+GPIO.setup(4, GPIO.OUT)
 
 #Asigno valor alto para que tenga corriente la salida de 3,3v por GPIO
-GPIO.output(1, GPIO.LOW)
+GPIO.output(4, GPIO.LOW)
 
 #Limpiar GPIO
 GPIO.cleanup()
