6589a80c3f26a0934a32872d56694ec21baf568d
nerviozzo <nerviozzo@nhckserver.nhck>
2015-08-06 14:18:39 +0200

    Copiado Botón 4 unas 20 veces para tener 1 botón por cada GPIO de los que vamos a usar. Aún no están configurado los nombres y pueden entrar en conflicto

--- Web para controlar GPIO/index.php
+++ Web para controlar GPIO/index.php
@@ -37,6 +37,161 @@
 			<INPUT type="submit" name="UNparpadeo4" value="1Parpadeo"/>
 			<INPUT type="submit" name="Parpadear4" value="Parpadear"/>
 		</FORM>
+
+
+
+
+<!--ACCIONES PARA GPIO2-->
+		<FORM action="" method="post">
+			GPIO 4 <INPUT type="submit" name="Activar4" value="Activar"/>
+			<INPUT type="submit" name="Desactivar4" value="Desactivar"/>
+			<INPUT type="submit" name="UNparpadeo4" value="1Parpadeo"/>
+			<INPUT type="submit" name="Parpadear4" value="Parpadear"/>
+		</FORM>
+		
+<!--ACCIONES PARA GPIO3-->
+		<FORM action="" method="post">
+			GPIO 4 <INPUT type="submit" name="Activar4" value="Activar"/>
+			<INPUT type="submit" name="Desactivar4" value="Desactivar"/>
+			<INPUT type="submit" name="UNparpadeo4" value="1Parpadeo"/>
+			<INPUT type="submit" name="Parpadear4" value="Parpadear"/>
+		</FORM>
+		
+<!--ACCIONES PARA GPIO4-->
+		<FORM action="" method="post">
+			GPIO 4 <INPUT type="submit" name="Activar4" value="Activar"/>
+			<INPUT type="submit" name="Desactivar4" value="Desactivar"/>
+			<INPUT type="submit" name="UNparpadeo4" value="1Parpadeo"/>
+			<INPUT type="submit" name="Parpadear4" value="Parpadear"/>
+		</FORM>
+		
+<!--ACCIONES PARA GPIO5-->
+		<FORM action="" method="post">
+			GPIO 4 <INPUT type="submit" name="Activar4" value="Activar"/>
+			<INPUT type="submit" name="Desactivar4" value="Desactivar"/>
+			<INPUT type="submit" name="UNparpadeo4" value="1Parpadeo"/>
+			<INPUT type="submit" name="Parpadear4" value="Parpadear"/>
+		</FORM>
+		
+<!--ACCIONES PARA GPIO6-->
+		<FORM action="" method="post">
+			GPIO 4 <INPUT type="submit" name="Activar4" value="Activar"/>
+			<INPUT type="submit" name="Desactivar4" value="Desactivar"/>
+			<INPUT type="submit" name="UNparpadeo4" value="1Parpadeo"/>
+			<INPUT type="submit" name="Parpadear4" value="Parpadear"/>
+		</FORM>
+		
+<!--ACCIONES PARA GPIO7-->
+		<FORM action="" method="post">
+			GPIO 4 <INPUT type="submit" name="Activar4" value="Activar"/>
+			<INPUT type="submit" name="Desactivar4" value="Desactivar"/>
+			<INPUT type="submit" name="UNparpadeo4" value="1Parpadeo"/>
+			<INPUT type="submit" name="Parpadear4" value="Parpadear"/>
+		</FORM>
+		
+<!--ACCIONES PARA GPIO8-->
+		<FORM action="" method="post">
+			GPIO 4 <INPUT type="submit" name="Activar4" value="Activar"/>
+			<INPUT type="submit" name="Desactivar4" value="Desactivar"/>
+			<INPUT type="submit" name="UNparpadeo4" value="1Parpadeo"/>
+			<INPUT type="submit" name="Parpadear4" value="Parpadear"/>
+		</FORM>
+		
+<!--ACCIONES PARA GPIO9-->
+		<FORM action="" method="post">
+			GPIO 4 <INPUT type="submit" name="Activar4" value="Activar"/>
+			<INPUT type="submit" name="Desactivar4" value="Desactivar"/>
+			<INPUT type="submit" name="UNparpadeo4" value="1Parpadeo"/>
+			<INPUT type="submit" name="Parpadear4" value="Parpadear"/>
+		</FORM>
+		
+<!--ACCIONES PARA GPIO10-->
+		<FORM action="" method="post">
+			GPIO 4 <INPUT type="submit" name="Activar4" value="Activar"/>
+			<INPUT type="submit" name="Desactivar4" value="Desactivar"/>
+			<INPUT type="submit" name="UNparpadeo4" value="1Parpadeo"/>
+			<INPUT type="submit" name="Parpadear4" value="Parpadear"/>
+		</FORM>
+		
+<!--ACCIONES PARA GPIO11-->
+		<FORM action="" method="post">
+			GPIO 4 <INPUT type="submit" name="Activar4" value="Activar"/>
+			<INPUT type="submit" name="Desactivar4" value="Desactivar"/>
+			<INPUT type="submit" name="UNparpadeo4" value="1Parpadeo"/>
+			<INPUT type="submit" name="Parpadear4" value="Parpadear"/>
+		</FORM>
+		
+<!--ACCIONES PARA GPIO12-->
+		<FORM action="" method="post">
+			GPIO 4 <INPUT type="submit" name="Activar4" value="Activar"/>
+			<INPUT type="submit" name="Desactivar4" value="Desactivar"/>
+			<INPUT type="submit" name="UNparpadeo4" value="1Parpadeo"/>
+			<INPUT type="submit" name="Parpadear4" value="Parpadear"/>
+		</FORM>
+		
+<!--ACCIONES PARA GPIO13-->
+		<FORM action="" method="post">
+			GPIO 4 <INPUT type="submit" name="Activar4" value="Activar"/>
+			<INPUT type="submit" name="Desactivar4" value="Desactivar"/>
+			<INPUT type="submit" name="UNparpadeo4" value="1Parpadeo"/>
+			<INPUT type="submit" name="Parpadear4" value="Parpadear"/>
+		</FORM>
+		
+<!--ACCIONES PARA GPIO14-->
+		<FORM action="" method="post">
+			GPIO 4 <INPUT type="submit" name="Activar4" value="Activar"/>
+			<INPUT type="submit" name="Desactivar4" value="Desactivar"/>
+			<INPUT type="submit" name="UNparpadeo4" value="1Parpadeo"/>
+			<INPUT type="submit" name="Parpadear4" value="Parpadear"/>
+		</FORM>
+		
+<!--ACCIONES PARA GPIO15-->
+		<FORM action="" method="post">
+			GPIO 4 <INPUT type="submit" name="Activar4" value="Activar"/>
+			<INPUT type="submit" name="Desactivar4" value="Desactivar"/>
+			<INPUT type="submit" name="UNparpadeo4" value="1Parpadeo"/>
+			<INPUT type="submit" name="Parpadear4" value="Parpadear"/>
+		</FORM>
+		
+<!--ACCIONES PARA GPIO16-->
+		<FORM action="" method="post">
+			GPIO 4 <INPUT type="submit" name="Activar4" value="Activar"/>
+			<INPUT type="submit" name="Desactivar4" value="Desactivar"/>
+			<INPUT type="submit" name="UNparpadeo4" value="1Parpadeo"/>
+			<INPUT type="submit" name="Parpadear4" value="Parpadear"/>
+		</FORM>
+		
+<!--ACCIONES PARA GPIO17-->
+		<FORM action="" method="post">
+			GPIO 4 <INPUT type="submit" name="Activar4" value="Activar"/>
+			<INPUT type="submit" name="Desactivar4" value="Desactivar"/>
+			<INPUT type="submit" name="UNparpadeo4" value="1Parpadeo"/>
+			<INPUT type="submit" name="Parpadear4" value="Parpadear"/>
+		</FORM>
+		
+<!--ACCIONES PARA GPIO18-->
+		<FORM action="" method="post">
+			GPIO 4 <INPUT type="submit" name="Activar4" value="Activar"/>
+			<INPUT type="submit" name="Desactivar4" value="Desactivar"/>
+			<INPUT type="submit" name="UNparpadeo4" value="1Parpadeo"/>
+			<INPUT type="submit" name="Parpadear4" value="Parpadear"/>
+		</FORM>
+		
+<!--ACCIONES PARA GPIO19-->
+		<FORM action="" method="post">
+			GPIO 4 <INPUT type="submit" name="Activar4" value="Activar"/>
+			<INPUT type="submit" name="Desactivar4" value="Desactivar"/>
+			<INPUT type="submit" name="UNparpadeo4" value="1Parpadeo"/>
+			<INPUT type="submit" name="Parpadear4" value="Parpadear"/>
+		</FORM>
+		
+<!--ACCIONES PARA GPIO20-->
+		<FORM action="" method="post">
+			GPIO 4 <INPUT type="submit" name="Activar4" value="Activar"/>
+			<INPUT type="submit" name="Desactivar4" value="Desactivar"/>
+			<INPUT type="submit" name="UNparpadeo4" value="1Parpadeo"/>
+			<INPUT type="submit" name="Parpadear4" value="Parpadear"/>
+		</FORM>
 	</BODY>
 </HTML>
 			
